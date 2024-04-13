ce81eeca3b929677c819e163d097286d810da509
Misa Takeda <misatakeda.2799@gmail.com>
2024-04-13 14:01:22 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// START section top dropdown btn
+function topDropdownFavo() {
+  document.getElementById("topDropdown").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches(".btnDropdown")) {
+    const dropdowns = document.getElementsByClassName("dropdownContent");
+    for (let i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+// END section top dropdown btn
