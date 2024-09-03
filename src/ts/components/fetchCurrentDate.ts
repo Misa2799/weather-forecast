9a5c4b005c5a94bdf12b30b6963db5bdd8c1da85
Misa Takeda <misatakeda.2799@gmail.com>
2024-09-03 13:49:35 -0700

    change a default language

--- src/ts/components/fetchCurrentDate.ts
+++ src/ts/components/fetchCurrentDate.ts
@@ -2,13 +2,14 @@ import { config } from "../apikey.js";
 import { CurrentData } from "../type/CurrentData.js";
 
 const apiKey: string = config.apiKey;
-// console.log("apiKey", apiKey);
-// let splittedCity: string = "";
+
+const selected: HTMLSelectElement | null = document.querySelector("#lang");
+const lang = selected?.value || "ja";
 
 //START fetch api
 export async function fetchCurrentDate(city: string): Promise<CurrentData> {
   // fetch
-  const forecastUrl: string = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  const forecastUrl: string = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&lang=${lang}`;
 
   const response = await fetch(forecastUrl);
   if (!response.ok) {
